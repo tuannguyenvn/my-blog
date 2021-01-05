20f388be1b6d49e6835224e939fc4eb3bb739f86
tuannguyenvn <tuannguyen.de@gmail.com>
2021-01-05 17:31:05 +0100

    make ArticlelList modular, add other ArtikelList in ArticlePage

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import articleContent from './article-content';
+import ArticlesList from '../components/ArticlesList';
+
 
 const ArticlePage = ({match}) => {
     const name = match.params.name;
     const article = articleContent.find(articles => articles.name === name);
+    const otherArticles = articleContent.filter(article => article.name !== name);
 
     if (!article) return <h1>Article does not exist!</h1>
 
@@ -12,7 +15,11 @@ const ArticlePage = ({match}) => {
         <h1>{article.title}</h1>
         {article.content.map((paragraph, key) => (
             <p key={key}>{paragraph}</p>
-        ))}        </>
+        ))}
+
+        <h3>Other Articles:</h3>
+        <ArticlesList articles={otherArticles} />         
+        </>
     );
 }
 export default ArticlePage;
--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import articles from './article-content';
 import articleContent from './article-content';
-import {Link} from 'react-router-dom';
+import ArticlesList from '../components/ArticlesList';
 
 const ArticlesListPage = () => (
     <>
-    <h1>Articles</h1>  
-        {articleContent.map((article,key) =>(
-             <Link className="article-list-item" key={key} to={`/article/${article.name}`}>
-                <h3>{article.title}</h3>
-                <p>{article.content[0].substring(0,150)}...</p>
-            </Link>
-        ))}
+    <h1>Articles</h1>
+    <ArticlesList articles={articleContent} />
  
     </>
 );
